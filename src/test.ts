094a862cd09998f654d1a20aa751dc33c4404cb0
zimingguo <zimingguo@tencent.com>
2024-08-05 18:18:22 +0800

    feat: add types

--- src/test.ts
+++ src/test.ts
@@ -1,28 +1,59 @@
-interface Todo {
-    title: string
-    description: string
-    completed: boolean
-  }
-  
+// /** 判断是否为奇数 */
+// type isOdd<T extends number> = `${T}` extends  `${number | ''}${ 1 | 3 | 5 | 7 | 9}` ? true : false
 
- type  MyPick<T, K extends keyof T> = {
-    [key in keyof T as Extract<key,K>]: T[key]
- }
+// /** 判断是否为偶数 */
+// type isEven<T extends number> = `${T}` extends  `${number | ''}${ 0 | 2 | 4 | 6 | 8}` ? true : false
 
-  type TodoPreview = MyPick<Todo, 'completed'| 'title'>
-  type dd = Extract<'title'| 'dd' | 'completed','completed'| 'title'>
-  
-  const todo: TodoPreview = {
-      title: 'Clean room',
-      completed: false,
-  }
-  
-type Item = 'semlinker,lolo,kakuqo';
 
-type Split<
-	T extends string, 
-	Delimiter extends string,
-> = T extends `${infer F}${Delimiter}${infer K}` ? [F, ...Split<K, Delimiter>] : [T]     
+// type CartesianProduct<A extends string | number> = A extends A ? [A] : never
 
-type ElementType = Split<Item, ','>; // ["semlinker", "lolo", "kakuqo"]
+// type Permutation<T, K = T> = [T] extends [never] ? [] : K extends K ? [K, ...Permutation<Exclude<T, K>>] : never
 
+// type perm = Permutation<'A' | 'B' | 'C'>; // ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']
+
+// type LengthOfString<T, K extends []> = T extends `${infer F}` ? K[] : []
+
+
+// type KebabCase<T extends string> = T extends `${infer F}${infer R}` ? 
+//     R extends Uncapitalize<R> 
+//     ? `${Uncapitalize<F>}${KebabCase<R>}` : `${Uncapitalize<F>}-${KebabCase<R>}`
+//     : T
+
+
+//     type CamelCase<T extends string>= T extends `${infer F}_${infer R}` ? `${F}${CamelCase<Capitalize<R>>}`: T
+    
+//     type camelCase1 = CamelCase<'foo__bar'> // expected to be 'foo_Bar'
+//     type camelCase2 = CamelCase<'HELLO_WORLD_WITH_TYPES'> // expected to be same as previous one
+
+
+//     type shiftArr<T extends any[]> = T extends [infer F, ...infer R] ? R : never
+//     type Zip<T extends any[], U extends any[]> = T extends [infer F, ...infer R] ? U extends {length: 0} ? [] : [[F,U[0]], ...Zip<R, shiftArr<U>>] : [] 
+
+//     type exp = Zip<[1, 2, 3], ['1', '2']> // expected to be [[1, true], [2, false]]
+
+
+//     type aa = "ROM" extends string ? true : false
+
+
+
+    type PersonInfo = {
+        name: 'Tom'
+        age: number
+        married: false
+        addr: {
+          home: '123456'
+          phone: '13111111111'
+        }
+        hobbies: ['sing', 'dance']
+        readonlyArr: readonly ['test']
+        fn: () => any
+      }
+
+      type ToPrimitive1<T extends {}> = {
+        [ key in keyof T ]: T[key] extends {} ? true : false
+      }
+
+
+      type ac = number extends {} ? 1: 2
+ 
+      type a = ToPrimitive1<PersonInfo>
--- src/utility-types/index.ts
+++ src/utility-types/index.ts
@@ -15,25 +15,52 @@ export type SimpleUnion<T extends {}> = {
 }
 
 /** 去除以提供的参数开头的对象属性 */
-type ExcludeObjProps<T extends object, K extends string> = {
+export type ExcludeObjProps<T extends object, K extends string> = {
     [key in keyof T as Exclude<key, key extends `${K}${infer F}` ? key : never>]: T[key]
 }
 
 // 遍历数组，比较当前元素和剩下元素的联合类型
 /** 判断元组中是否包含了重复元素 */
-type CheckRepeatedTuple<T extends any[]> =  T extends [infer F, ...infer R] ? F extends R[number] ? true : CheckRepeatedTuple<R> : false
-
+export type CheckRepeatedTuple<T extends any[]> =  T extends [infer F, ...infer R] ? F extends R[number] ? true : CheckRepeatedTuple<R> : false
 
 type Merge<O1, O2> = {
     [key in keyof O1 | keyof O2]: PropOr<O1, key> | PropOr<O2, key>;
 }
 
 
+// 获取对象属性
 type PropOr<Obj, Prop> = Prop extends keyof Obj
   ? Obj[Prop]
   : never
 
-
-type MergeAll<T extends {}[], Prev extends {} =T > = T extends [infer F, ...infer R extends {}[]] ? MergeAll<R, Merge<F, Prev>> : Prev
+/** 合并对象，相同属性的值合并 */
+export type MergeAll<T extends {}[], Prev extends {} =T > = T extends [infer F, ...infer R extends {}[]] ? MergeAll<R, Merge<F, Prev>> : Prev
 
  
+/** 判断是否为奇数 */
+export type isOdd<T extends number> = `${T}` extends  `${number | ''}${ 1 | 3 | 5 | 7 | 9}` ? true : false
+
+/** 判断是否为偶数 */
+export type isEven<T extends number> = `${T}` extends  `${number | ''}${ 0 | 2 | 4 | 6 | 8}` ? true : false
+
+
+/** 获取一个联合类型的排列组合 */
+export type Permutation<T, K = T> = [T] extends [never] ? [] : K extends K ? [K, ...Permutation<Exclude<T, K>>] : never
+
+
+type StringToArray<T extends string> = T extends `${infer F}${infer R}` ? [F, ...StringToArray<R>] : []
+
+export type StringLength<S extends string> = StringToArray<S>['length']
+
+
+export type StringToUnion<T extends string> = T extends `${infer F}${infer R}` ? F | StringToUnion<R> : never
+
+/** 驼峰 转 -类型 */
+export type KebabCase<T extends string> = T extends `${infer F}${infer R}` ? 
+    R extends Uncapitalize<R> 
+    ? `${Uncapitalize<F>}${KebabCase<R>}` : `${Uncapitalize<F>}-${KebabCase<R>}`
+    : T
+
+type shiftArr<T extends any[]> = T extends [infer F, ...infer R] ? R : never
+
+export type Zip<T extends any[], U extends any[]> = T extends [infer F, ...infer R] ? U extends {length: 0} ? [] : [[F,U[0]], ...Zip<R, shiftArr<U>>] : [] 
